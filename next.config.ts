08cd904b4d49670675d8b198bd6b3ccc3d9110c9
kerolosatef1 <atefkerolos25@gmail.com>
2026-03-29 04:26:09 +0200

    Done Protfolio WITH next&Gsap&TAILWIND&Shadcn/UI

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts")
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
